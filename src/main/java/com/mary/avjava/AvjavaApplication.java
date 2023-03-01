0b534d58eafa618f64aed0a5db463660ae34b41e
Maryangela <mary.soarez@gmail.com>
2023-03-01 17:12:23 -0300

    Projeto Criado

---
+++ src/main/java/com/mary/avjava/AvjavaApplication.java
@@ -0,0 +1,13 @@
+package com.mary.avjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvjavaApplication.class, args);
+	}
+
+}
